8fdab026b66305af26e983ae2d8a1575978dab31
KunigiMohan <kmohanr5@gmail.com>
2021-12-23 17:14:12 +0530

    Added : Main class of EmployeePayrollAppApplication.

---
+++ src/main/java/com/application/EmployeePayrollApp/EmployeePayrollAppApplication.java
@@ -0,0 +1,15 @@
+package com.application.EmployeePayrollApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @SpringBootApplication : main application class
+ */
+@SpringBootApplication
+public class EmployeePayrollAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+	}
+}
