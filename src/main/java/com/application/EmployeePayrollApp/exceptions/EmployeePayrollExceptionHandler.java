74a71478d7d9df55c530aff9818cf3f375a7209f
KunigiMohan <kmohanr5@gmail.com>
2021-12-25 17:17:39 +0530

    Added : Class to Handle the Exceptions.

---
+++ src/main/java/com/application/EmployeePayrollApp/exceptions/EmployeePayrollExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.application.EmployeePayrollApp.exceptions;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import com.application.EmployeePayrollApp.DTO.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * @ControllerAdvice : to handle all Exception throw in application
+ * @ExceptionHandler : to declare a method to handle particular Exception
+ */
+
+@ControllerAdvice
+public class EmployeePayrollExceptionHandler {
+    
+    /**
+     * method to handle any Exception thrown
+     * @param exception
+     * @return : ResponseEntity of Exception
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+        List<String> errMsg = errorList.stream().map(objErr -> objErr.getDefaultMessage()).collect(Collectors.toList());
+        ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request ", errMsg);
+        return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.BAD_REQUEST);
+    }
+}
