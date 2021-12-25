2fbc721f59324283c0d7e9a7197abcbc254d219c
KunigiMohan <kmohanr5@gmail.com>
2021-12-25 17:40:15 +0530

    Added : Creating Custom Exception when Employee Id is not found.

---
+++ src/main/java/com/application/EmployeePayrollApp/exceptions/EmployeePayrollException.java
@@ -0,0 +1,11 @@
+package com.application.EmployeePayrollApp.exceptions;
+
+
+/**
+ * Creating custom Exception
+ */
+public class EmployeePayrollException extends RuntimeException{
+    public EmployeePayrollException(String message){
+        super(message);
+    }
+}
