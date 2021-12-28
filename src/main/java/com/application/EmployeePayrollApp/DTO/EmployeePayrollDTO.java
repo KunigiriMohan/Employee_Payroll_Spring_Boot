edfaae1c68d1fd03ed6a240865c2d0bb2257de25
KunigiMohan <kmohanr5@gmail.com>
2021-12-28 13:36:42 +0530

    Added : Adding Reamining Variables of Employee.

--- src/main/java/com/application/EmployeePayrollApp/DTO/EmployeePayrollDTO.java
+++ src/main/java/com/application/EmployeePayrollApp/DTO/EmployeePayrollDTO.java
@@ -1,25 +1,30 @@
 package com.application.EmployeePayrollApp.DTO;
 
+
+import java.time.LocalDate;
+import java.util.List;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.ToString;
+
+
 /**
  * @Min : To set minimum value of variable
  * @Valid :To check entered variable is valid or not
+ * @JsonFormat : to define date format
  */
-public class EmployeePayrollDTO {
+
+public @ToString class EmployeePayrollDTO {
     @Pattern (regexp = "^[A-Z]{1}[a-zA-Z]{2,}$", message = "Employee name is Invalid")
     public String name;
 
     @Min(value =500,message = "Min Wage Should be more than 500")
     public long salary;
-    
-    /**
-     * Defining Constructer
-     * @param name
-     * @param salary
-     */
-    public EmployeePayrollDTO(String name, long salary) {
-        this.name = name;
-        this.salary = salary;
-    }
+    public String gender;
+    @JsonFormat(pattern = "dd MM yyy")
+    public LocalDate startDate;
+    public String note;
+    public String profilePic;
+    public List<String> department;
 }
