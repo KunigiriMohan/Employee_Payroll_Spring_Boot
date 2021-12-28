fc0a7c007509a99b69147dcb2016c3f6b10e6491
KunigiMohan <kmohanr5@gmail.com>
2021-12-28 20:00:41 +0530

    Added : Creating Query method to get data by Department.

--- src/main/java/com/application/EmployeePayrollApp/repository/EmployeePayrollRepository.java
+++ src/main/java/com/application/EmployeePayrollApp/repository/EmployeePayrollRepository.java
@@ -1,7 +1,10 @@
 package com.application.EmployeePayrollApp.repository;
+import java.util.List;
+
 import com.application.EmployeePayrollApp.model.*;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Repository;
  * EmployeePayrollRepository to create repository of JpaRepository
  */
 @Repository
-public interface EmployeePayrollRepository extends JpaRepository<EmployeePayrollData,Integer>{
-    
+public interface EmployeePayrollRepository extends JpaRepository<EmployeePayrollData,Long>{
+    @Query (value = "select * from employee_table , employee_department where employee_id = id and department = :department",nativeQuery = true)
+    List<EmployeePayrollData> findEmployeesByDepartment(String department);
 }
