3af9bfbf9a67b6ac7c368c10df5c6ffcbf70e395
KunigiMohan <kmohanr5@gmail.com>
2021-12-28 13:38:24 +0530

    Added : Adding Remaining details of Employee.

--- src/main/java/com/application/EmployeePayrollApp/model/EmployeePayrollData.java
+++ src/main/java/com/application/EmployeePayrollApp/model/EmployeePayrollData.java
@@ -1,16 +1,24 @@
 package com.application.EmployeePayrollApp.model;
 
-import javax.persistence.GeneratedValue;
-
+import java.time.LocalDate;
+import java.util.List;
 import com.application.EmployeePayrollApp.DTO.EmployeePayrollDTO;
-
 import lombok.Data;
 
-public @Data class EmployeePayrollData {
-	@GeneratedValue
-	private long id;
-    private EmployeePayrollDTO employeePayrollDTO;
 
+/**
+ * @Data : Using Lombok to Generate getters and setters
+ * @GeneratedValue : To auto generate i value
+ */
+public @Data class EmployeePayrollData {
+	private long id;
+	private String name;
+	private long salary;
+	private String gender;
+	private LocalDate startDate;
+	private String note;
+	private String profilePic;
+	private List<String> department;
 	/**
 	 * Defining Constructer
 	 * @param id
@@ -20,6 +28,20 @@ public @Data class EmployeePayrollData {
 	
 	public EmployeePayrollData(int id, EmployeePayrollDTO employeePayrollDTO){
 		this.id=id;
-		this.employeePayrollDTO=employeePayrollDTO;
+		this.updateEmployeeDetails(employeePayrollDTO);
+		
+	}
+	/**
+	 * Constructor to define all variables from DTO object
+	 * @param employeePayrollDTO
+	 */
+	private void updateEmployeeDetails(EmployeePayrollDTO employeePayrollDTO) {
+		this.name = employeePayrollDTO.name;
+		this.salary=employeePayrollDTO.salary;
+		this.gender=employeePayrollDTO.gender;
+		this.startDate=employeePayrollDTO.startDate;
+		this.note=employeePayrollDTO.note;
+		this.profilePic=employeePayrollDTO.profilePic;
+		this.department=employeePayrollDTO.department;
 	}
 }
