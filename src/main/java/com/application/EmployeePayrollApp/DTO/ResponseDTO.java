12d341431b715381684b8b3c41e0e1aead903c84
KunigiMohan <kmohanr5@gmail.com>
2021-12-29 13:45:23 +0530

    Added : Using Annotation to Generate Constructors.

--- src/main/java/com/application/EmployeePayrollApp/DTO/ResponseDTO.java
+++ src/main/java/com/application/EmployeePayrollApp/DTO/ResponseDTO.java
@@ -1,20 +1,17 @@
 package com.application.EmployeePayrollApp.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 /**
  * @Data : Using lombok to autogenerate toString() , Getters and Setters.
+ * @AllArgsContructor: To Create Constructor
+ * @NoArgsConstructor : To Create No Constructor
  */
-public @Data class ResponseDTO {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public  class ResponseDTO {
     public String message;
-    public Object data;
-    
-    /**
-     * Defining Constructor
-     * @param name
-     * @param employeePayrollData
-     */
-    public ResponseDTO(String message, Object employeePayrollData) {
-        this.message = message;
-        this.data = employeePayrollData;
-    }   
+    public Object data;  
 }
