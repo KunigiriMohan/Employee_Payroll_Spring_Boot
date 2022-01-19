eeac48643b0ac08cbb1702fbb89f8ca853da8225
kunigirimohan <kmohanr5@gmail.com>
2022-01-19 09:45:48 +0530

    Added : Refactoring method to verify JWT and generate Token.

--- src/main/java/com/application/EmployeePayrollApp/service/InterfaceEmployeeservice.java
+++ src/main/java/com/application/EmployeePayrollApp/service/InterfaceEmployeeservice.java
@@ -2,15 +2,17 @@ package com.application.EmployeePayrollApp.service;
 import java.util.List;
 import com.application.EmployeePayrollApp.DTO.EmployeePayrollDTO;
 import com.application.EmployeePayrollApp.model.EmployeePayrollData;
+import com.application.EmployeePayrollApp.model.User;
 
 /**
  * Interface to create methods to perform operations
  */
 public interface InterfaceEmployeeservice {
-    List<EmployeePayrollData> getEmployeePayrollData();
-    EmployeePayrollData getEmployeePayrollDataById(Long empId);
-    EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO);
-    void deleteEmployeePayrollData(Long empId);
-    EmployeePayrollData updateEmployeePayrollData(Long empID, EmployeePayrollDTO employeePayrollDTO);
-    List<EmployeePayrollData> getEmployeesByDepartment(String department);
+    List<EmployeePayrollData> getEmployeePayrollData(String token);
+    EmployeePayrollData getEmployeePayrollDataById(Long empId,String token);
+    EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO,String token);
+    void deleteEmployeePayrollData(Long empId,String token);
+    EmployeePayrollData updateEmployeePayrollData(Long empID, EmployeePayrollDTO employeePayrollDTO,String token);
+    public User createUser(User user);
+    public String generateToken(User user);
 }
