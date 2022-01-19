b5225cb0faffdfa6195d8ee6f0b43a0aa129ef1a
kunigirimohan <kmohanr5@gmail.com>
2022-01-19 09:42:07 +0530

    Added : Creating method to check User is present in Database.

--- src/main/java/com/application/EmployeePayrollApp/repository/EmployeePayrollRepository.java
+++ src/main/java/com/application/EmployeePayrollApp/repository/EmployeePayrollRepository.java
@@ -1,9 +1,7 @@
 package com.application.EmployeePayrollApp.repository;
 
-import java.util.List;
 import com.application.EmployeePayrollApp.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -13,6 +11,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface EmployeePayrollRepository extends JpaRepository<EmployeePayrollData,Long>{
-    @Query (value = "select * from employee_table , employee_department where employee_id = id and department = :department",nativeQuery = true)
-    List<EmployeePayrollData> findEmployeesByDepartment(String department);
+    
 }
